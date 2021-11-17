57158ebba950acf3e26eca0537e9ffb36a606a7a
Vehmeyer <j.vehmeyer@gmail.com>
2021-11-17 11:51:43 -0600

    added notes for array methods

--- Arrays.js
+++ Arrays.js
@@ -1,10 +1,71 @@
-// .length
-// .push()
-// .pop()
-// .unshift()
-// .shift()
-// .forEach()
-// .map()
-// .reduce()
-// .filter()
+const array = [
+    {"city":"seattle", "state":"WA", "population":652405, "land_area":83.9},
+    {"city":"new york", "state":"NY", "population":8405837, "land_area":302.6},
+    {"city":"boston", "state":"MA", "population":645966, "land_area":48.3},
+    {"city":"kansas city", "state":"MO", "population":467007, "land_area":315}
+  ];
+
+// access first item in array
+
+console.log(array[0]);
+
+// access last item in array
+
+console.log(array[array.length - 1]);
+
+// .length - returns number of items in array
+
+array.length 
+
+// .push() - adds item to end of array and returns new length
+
+array.push()
+
+// .pop() - removes last item of array and returns the "popped" item
+
+array.pop()
+
+// .unshift() - adds item to beginning of array
+
+array.unshift()
+
+// .shift() - removes item from beginning of array
+
+array.shift()
+
+// .forEach() - iterates through each item in array
+
+array.forEach(function(item) {
+    return item.key;
+});
+
+array.forEach(item => item.key);
+
+// .map() - converts array into new array
+
+const newArray = array.map(function(item){
+    return item.key.toLowerCase();
+});
+
+const newArray = array.map(item => item.key.toLowerCase());
+
+// .reduce() - returns a single value from array, can use for multiplication or addition
+
+const singleValue = array.reduce(function(accumulator, item){
+    return accumulator + item;
+}, 0); 
+    // input 0 for addition, 1 for multiplication
+
+const singleValue = array.reduce((accumulator, item) => {
+    return accumulator + item;
+}, 0);
+
+// .filter() - filters array based on something that is true or false
+
+const newArray = array.filter(function(item){
+    return item.population > 10;
+});
+
+const newArray = array.filter(item => item.population > 10);
+
 // .find()
