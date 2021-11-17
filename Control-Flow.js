ce43e00ccd8547aac8636470545dedaaf6caac23
Vehmeyer <j.vehmeyer@gmail.com>
2021-11-17 11:07:00 -0600

    wip

---
+++ Control-Flow.js
@@ -0,0 +1,9 @@
+// 1. If > Else If > Else
+
+// 2. For
+
+// 3. While
+
+// 4. Do While
+
+// 5. Switch
---
+++ Functions.js
@@ -0,0 +1,17 @@
+// 1. Function Declaration
+
+function add(a,b) {
+    console.log(a + b);
+}
+
+// 2. Function Expression
+
+const add = function(a,b) {
+    console.log(a + b);
+}
+
+// 3. Arrow Functions
+
+const add = (a,b) => {
+    console.log(a + b)
+}
